e7abca95df0ca8cc4bdafb923ab05fd76d28f0c5
uujeen <rkddmlwls2@gmail.com>
2023-07-15 03:30:52 +0900

    feat: complete animation

--- 02/basic/src/main.js
+++ 02/basic/src/main.js
@@ -1,6 +1,7 @@
 // import examle from './ex01';
 // import examle from './ex02';
 // import examle from './ex03';
-import examle from './ex04';
+// import examle from './ex04';
+import examle from './ex05';
 
 examle();
